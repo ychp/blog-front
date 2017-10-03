440752fe91f6a9a21c725d7d6b140e4660a8bff2
应程鹏 <ychp@terminus.io>
2017-10-03 13:29:43 +0800

    update nav

--- src/components/common/nav/view.vue
+++ src/components/common/nav/view.vue
@@ -9,7 +9,7 @@
             <ul class="nav-items close">
                 <i class="iconfont icon-liebiao2 small-nav" v-on:click="toggleNav($event)"></i>
                 <i class="iconfont icon-31guanbi small-nav-close" v-on:click="toggleNav($event)"></i>
-                <li class="nav-item" v-for="(menu, index) in menus" v-bind:class = "{ last: index == menus.length - 1 }">
+                <li class="nav-item" v-for="(menu, index) in menus">
                     <a class="nav-link" v-bind:href="menu.url" v-bind:class="{ active: currentPath == menu.url }">
                         {{ menu.title }}
                     </a>
